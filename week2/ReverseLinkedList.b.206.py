e3a908f5a5632a0e98f6a2f5cb6936ebd430a080
lucas-nunes-matheus <lucas.matheus@sou.inteli.edu.br>
2025-08-11 22:49:24 -0300

    template: add a template proposal to implement another solution to reverse linked list 206

---
+++ week2/ReverseLinkedList.b.206.py
@@ -0,0 +1,10 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        Aqui, fica aberto para resolução futura. Não deixe de fazer
+        '''
